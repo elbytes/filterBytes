b20c82e2cf00f841dbc66bfb92f2c3afb2f58d0e
El <el@Els-iMac.local>
2021-02-26 08:40:25 -0800

    sepia func -  vintage adn noir btns

--- filterBytes/ViewController.swift
+++ filterBytes/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     let context = CIContext()
+    var original: UIImage?
     
     @IBOutlet weak var displayImage: UIImageView!
     
@@ -28,12 +29,28 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         navigationController?.dismiss(animated: true, completion: nil)
         if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
             displayImage.image = image
+            original = image
         }
     }
     
   
     @IBAction func sepia(_ sender: Any) {
+        guard let original = original else {
+            return
+        }
         let filter = CIFilter(name: "sepiaFilter")
+        filter?.setValue(0.5, forKey: kCIInputIntensityKey)
+        filter?.setValue(CIImage(image: original), forKey: kCIInputImageKey)
+        let output = filter?.outputImage
+        displayImage.image = UIImage(cgImage: self.context.createCGImage(output!, from: output!.extent)!)
+    }
+    
+    
+    @IBAction func noir(_ sender: Any) {
+        
+    }
+    
+    @IBAction func vintage(_ sender: Any) {
     }
     
   override func viewDidLoad() {
