664b9ba5994d8635624a1423b921307ebcbf86d4
El <el@Els-iMac.local>
2021-02-26 10:42:40 -0800

    noir and vintage filter funcs

--- filterBytes/ViewController.swift
+++ filterBytes/ViewController.swift
@@ -40,17 +40,30 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }
         let filter = CIFilter(name: "sepiaFilter")
         filter?.setValue(0.5, forKey: kCIInputIntensityKey)
-        filter?.setValue(CIImage(image: original), forKey: kCIInputImageKey)
-        let output = filter?.outputImage
-        displayImage.image = UIImage(cgImage: self.context.createCGImage(output!, from: output!.extent)!)
+        display(filter: filter!)
     }
     
     
     @IBAction func noir(_ sender: Any) {
-        
+        guard let original = original else {
+            return
+        }
+        let filter = CIFilter(name: "noirFilter")
+        display(filter: filter!)
     }
     
     @IBAction func vintage(_ sender: Any) {
+        guard let original =  original else {
+            return
+        }
+        let filter = CIFilter(name: "vintageFilter")
+        display(filter: filter!)
+    }
+    
+    func display(filter: CIFilter){
+        filter.setValue(CIImage(image: original!), forKey: kCIInputImageKey)
+        let output = filter.outputImage
+        displayImage.image = UIImage(cgImage: self.context.createCGImage(output!, from: output!.extent)!)
     }
     
   override func viewDidLoad() {
